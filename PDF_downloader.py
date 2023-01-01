0cda3521262060cc8c30c7c2060a03e4fe788158
Aniol <aniol0012@gmail.com>
2023-01-01 20:51:29 +0100

    v1.1
    Minor change

--- PDF_downloader.py
+++ PDF_downloader.py
@@ -3,6 +3,7 @@ import urllib.request
 # Set the URL of the file and the path where we want to save it
 link = "https://www.example.com/test.pdf"
 
+# Set the path to download (remember to use double backslash "\\" instead of single one)
 file_path = "C:\\Users\\%USERPROFILE%\\Desktop"
 
 # Download the file and save it in the specified path

bd7efb7fe0db264458ae33eeb5ffab8acb7315e3
Aniol <aniol0012@gmail.com>
2023-01-01 21:31:43 +0100

    v2
    - Multiple links
    - Better looking

--- PDF_downloader.py
+++ PDF_downloader.py
@@ -1,10 +1,31 @@
 import urllib.request
 
-# Set the URL of the file and the path where we want to save it
-link = "https://www.example.com/test.pdf"
-
 # Set the path to download (remember to use double backslash "\\" instead of single one)
-file_path = "C:\\Users\\%USERPROFILE%\\Desktop"
+file_path = ".\\"
 
-# Download the file and save it in the specified path
-urllib.request.urlretrieve(link, file_path)
+# Example
+#file_path = "C:\\Users\\YOUR_USER\\Desktop"
+
+# Set the URL's of the files
+links = [ # CHANGE THIS LINKS
+    "https://www.example.com/test_1.pdf",
+    "https://www.example.com/test_2.pdf",
+    "https://www.example.com/test_3.pdf",
+    "https://www.example.com/test_4.pdf",
+    "https://www.example.com/test_5.pdf",
+    "https://www.example.com/test_6.pdf",
+    "https://www.example.com/test_7.pdf",
+]
+
+# Do not modify under this
+file_number = 0
+
+print("Downloading files:")
+
+for link in links:
+    file_number += 1
+    print(file_number, "-", link)
+    # Sets the url name of the file
+    file_name = link.split("/")[-1]
+    file_path_w_name = file_path + file_name
+    urllib.request.urlretrieve(link, file_path_w_name)

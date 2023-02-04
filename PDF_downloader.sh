11f5bbc85f297ec76fa27d0da1871b968b9d69fc
Aniol Serrano <53788631+Aniol0012@users.noreply.github.com>
2023-02-04 13:07:50 +0100

    For linux and Mac

---
+++ PDF_downloader.sh
@@ -0,0 +1,26 @@
+# Author: Aniol0012
+# Date: Feburary 2023
+
+links=(
+    "https://www.example.com/test_1.pdf"
+    "https://www.example.com/test_2.pdf"
+    "https://www.example.com/test_3.pdf"
+    "https://www.example.com/test_4.pdf"
+    "https://www.example.com/test_5.pdf"
+    "https://www.example.com/test_6.pdf"
+    "https://www.example.com/test_7.pdf"
+)
+
+if [[ $1 == "-r" ]]; then
+    rm -rf *.pdf
+    echo "All pdf files removed"
+    ls
+else
+    echo "Printing all links..."
+
+    for link in ${links[@]}; do
+        curl -O $link >/dev/null 2>&1
+    done
+
+    echo "Done"
+fi
